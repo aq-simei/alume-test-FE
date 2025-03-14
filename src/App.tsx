aa938c05a055c534d62f58a435b10f58b8dbba53
simei <aq.simei@gmail.com>
2025-03-13 22:47:53 -0300

    feat: add new wrappers on app for theme, toast and query client

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { RouterProvider } from "react-router";
 import { router } from "./router/routes";
+import { ThemeProvider } from "./components/theme/provider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "./components/ui/sonner";
 
 const App = () => {
+  const queryClient = new QueryClient();
   return (
-    <HelmetProvider>
-      <Helmet titleTemplate="%s | Space Dash"/>
-      <RouterProvider router={router} />
-    </HelmetProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider defaultTheme="light" storageKey="theme">
+        <HelmetProvider>
+          <Toaster />
+          <Helmet titleTemplate="%s | Space Dash" />
+          <RouterProvider router={router} />
+        </HelmetProvider>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 };
 
