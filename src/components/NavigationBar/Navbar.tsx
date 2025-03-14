7441604f4b5bae0b1696789dd000f2a2bbb60e00
simei <aq.simei@gmail.com>
2025-03-13 22:47:15 -0300

    fix: update app layout with navbar

---
+++ src/components/NavigationBar/Navbar.tsx
@@ -0,0 +1,58 @@
+import { CircleHelp, Home } from "lucide-react";
+import { ModeToggle } from "../theme/toggler";
+import { Link, useLocation } from "react-router-dom";
+import { Button } from "../ui/button";
+import { twMerge } from "tailwind-merge";
+
+const paths = [
+  {
+    name: "Home",
+    icon: Home,
+    path: "/",
+  },
+  {
+    name: "About",
+    icon: CircleHelp, // Replace with appropriate icon
+    path: "/about",
+  },
+  {
+    name: "Bookings",
+    icon: Home, // Replace with appropriate icon
+    path: "/bookings",
+  },
+];
+
+export const Navbar = () => {
+  const location = useLocation();
+  return (
+    <nav className="flex flex-row w-full px-4">
+      <div className="flex flex-row  min-h-10 w-full justify-evenly">
+        <ul className="flex items-center w-full justify-start space-x-2">
+          {paths.map(({ name, path, icon: Icon }) => (
+            <li key={name}>
+              <Link to={path}>
+                <Button
+                  variant={"link"}
+                  className={twMerge(
+                    "hover:cursor-pointer font-bold hover:bg-purple-400/50 transition-all duration-500"
+                  )}
+                >
+                  <Icon
+                    size={18}
+                    stroke={
+                      path === location.pathname ? "currentColor" : "none"
+                    }
+                  />
+                  {name}
+                </Button>
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <div className="flex items-center">
+          <ModeToggle />
+        </div>
+      </div>
+    </nav>
+  );
+};
--- src/pages/App/_layout.tsx
+++ src/pages/App/_layout.tsx
@@ -1,10 +1,30 @@
+import { Navbar } from "@/components/NavigationBar/Navbar";
+import { Rocket } from "lucide-react";
 import { Outlet } from "react-router";
 
 export const AppLayout = () => {
   return (
-    <div>
-      <h1>On app layout</h1>
-      <Outlet />
-    </div>
+    <>
+      <div className="w-screen h-screen flex flex-col bg-background">
+        <div className="flex-grow flex-shrink-0 mt-4">
+          <Navbar />
+          <Outlet />
+        </div>
+        <footer className="border-t border-gray-800 py-8">
+          <div className="container">
+            <div className="flex flex-col md:flex-row justify-between items-center">
+              <div className="flex items-center gap-2 mb-4 md:mb-0">
+                <Rocket className="h-6 w-6 text-primary" />
+                <span className="text-lg font-bold">Space Dash</span>
+              </div>
+              <div className="text-sm text-gray-400">
+                &copy; {new Date().getFullYear()} SpaceVoyager. All rights
+                reserved. In partnership with SpaceX.
+              </div>
+            </div>
+          </div>
+        </footer>
+      </div>
+    </>
   );
 };
