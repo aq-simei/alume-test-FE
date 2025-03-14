b704094804276b150049b593ad26aa7d773b0385
simei <aq.simei@gmail.com>
2025-03-14 19:40:27 -0300

    feat: update BookingContext logic

--- src/contexts/BookingContext.tsx
+++ src/contexts/BookingContext.tsx
@@ -1,10 +1,14 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useMemo, useState } from "react";
 
 interface BookingContextType {
   userName: string;
   userAge: number;
   flightNumber: string;
-  healthComplications: boolean;
+  healthComplications: boolean | null;
+  updateUserName: (name: string) => void;
+  updateUserAge: (age: number) => void;
+  updateFlightNumber: (flight: string) => void;
+  updateHealthComplications: (complications: boolean) => void;
 }
 
 export const BookingContext = createContext<BookingContextType | undefined>(
@@ -12,15 +16,39 @@ export const BookingContext = createContext<BookingContextType | undefined>(
 );
 
 export const BookingProvider = ({ children }: { children: ReactNode }) => {
+  const [userName, setUserName] = useState<string>("");
+  const [userAge, setUserAge] = useState<number>(0);
+  const [flightNumber, setFlightNumber] = useState<string>("");
+  const [healthComplications, setHealthComplications] = useState<
+    boolean | null
+  >(null);
+
+  const updateUserName = (name: string) => setUserName(() => name);
+  const updateUserAge = (age: number) => setUserAge(() => age);
+  const updateFlightNumber = (flight: string) => setFlightNumber(() => flight);
+  const updateHealthComplications = (complications: boolean) =>
+    setHealthComplications(() => complications);
+  const bookingData = useMemo(
+    () => ({
+      userName,
+      userAge,
+      flightNumber,
+      healthComplications,
+      updateUserName,
+      updateUserAge,
+      updateFlightNumber,
+      updateHealthComplications,
+    }),
+    [
+      flightNumber,
+      healthComplications,
+      userName,
+      userAge,
+    ]
+  );
+
   return (
-    <BookingContext.Provider
-      value={{
-        userName: "John Doe",
-        userAge: 30,
-        flightNumber: "BA123",
-        healthComplications: false,
-      }}
-    >
+    <BookingContext.Provider value={bookingData}>
       {children}
     </BookingContext.Provider>
   );
